b7604e3672fac7d33262b1dfdfbb0318a2a643ec
DESKTOP-CLD0RVR\Nat <foxdhalia@gmail.com>
2018-01-27 20:47:32 -0200

    ícones de vida aparecem e desaparecem conforme a vida do player; Menus (limpos), tela inicial, intro, game over.

--- GameJam2018/Assets/Scripts/Life.cs
+++ GameJam2018/Assets/Scripts/Life.cs
@@ -1,18 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+
 
 public class Life : MonoBehaviour {
     public Transform lifes;
-    public int qntLifes = 3;
+    public int qntLifes;
+
+    public Image[] rocket = new Image[3];
+    public GameObject gameOverPanel;
+    
+    
+
 	// Use this for initialization
 	void Start () {
-	
 
+        qntLifes = 3;
+        for (int i=0; i<3; i++)
+        {
+            rocket[i].enabled = true;
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		if (qntLifes == 3 && !rocket[2].enabled)
+        {
+            rocket[2].enabled = true;
+        }
+        else if (qntLifes == 2 && rocket[2].enabled)
+        {
+            rocket[2].enabled = false;            
+        }
+        else if (qntLifes == 1 && rocket[1].enabled)
+        {
+            rocket[1].enabled = false;
+        }
+        else if (qntLifes == 2 && !rocket[1].enabled)
+        {
+            rocket[1].enabled = true;
+        }
+        else if (qntLifes <= 0 && !rocket[0].enabled)
+        {
+            rocket[0].enabled = false;
+           
+        }
+
+        if(qntLifes <= 0)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                rocket[i].enabled = false;
+            }
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.active = true;
+    }
 }
---
+++ GameJam2018/Assets/UI/Scripts/ButtonsConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsConfig : MonoBehaviour {
+
+    public void GoToIntro()
+    {
+        Application.LoadLevel("Intro");
+    }
+
+    public void GameStart()
+    {
+        Application.LoadLevel("GamePlay");
+    }
+
+    public void ReturnToMenu()
+    {
+        Application.LoadLevel("Menu");
+    }
+}
